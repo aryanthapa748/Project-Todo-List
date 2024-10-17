a79ac984af5966232e549923d566feeb0f13a8db
Aryan Thapa <aryanthapa748@gmail.com>
2024-10-16 20:48:42 -0400

    Adding template literal and Date object to make id more unique

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ openTaskFormBtn.addEventListener('click', () =>{
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 
   const taskObj = {
-    id: titleInput.value,
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
     
   };
   console.log(taskObj);
